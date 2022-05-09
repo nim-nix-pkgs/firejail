c73e164fdca1a7368b043596882d561c3a628bea
hugosenari <hugosenari@gmail.com>
2022-05-09 20:37:18 -0300

    chore: re index firejail

--- flake.nix
+++ flake.nix
@@ -7,14 +7,6 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs."firejail-master".dir   = "master";
-  inputs."firejail-master".owner = "nim-nix-pkgs";
-  inputs."firejail-master".ref   = "master";
-  inputs."firejail-master".repo  = "firejail";
-  inputs."firejail-master".type  = "github";
-  inputs."firejail-master".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."firejail-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
